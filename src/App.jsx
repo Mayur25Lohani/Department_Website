b51122d193f3f10b6c060b755a5e7127eba9d43f
Mayur25Lohani <lohanimayuresh2551@gmail.com>
2024-01-01 21:36:17 +0530

    Added Contact Form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav from './components/nav/Nav'
 import Cover from './components/cover/cover'
 import Footer from './components/footer/Footer'
 import Resources from './components/resources/Resources'
+import Contact from './components/contact/Contact'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
     <Nav/>
     <Cover/>
     <Resources />
+    <Contact />
     <Footer />
     </>
   )
